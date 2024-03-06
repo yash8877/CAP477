7d70a067a14af07e79722c215a10cd495adba69a
Yash Raj <yash524767@gmail.com>
2024-03-07 00:48:20 +0530

    Added the ques-9

---
+++ Unit-3/inheritance and polymorphism/i-desgin/ques-9/TicketBooking.java
@@ -0,0 +1,45 @@
+public class TicketBooking {
+    private String stageEvent;
+    private String customer;
+    private Integer noOfSeats;
+
+    public TicketBooking() {
+        this.stageEvent = "";
+        this.customer = "";
+        this.noOfSeats = 0;
+    }
+
+    public TicketBooking(String stageEvent, String customer, Integer noOfSeats) {
+        this.stageEvent = stageEvent;
+        this.customer = customer;
+        this.noOfSeats = noOfSeats;
+    }
+
+    public String getStageEvent() {
+        return stageEvent;
+    }
+
+    public void setStageEvent(String stageEvent) {
+        this.stageEvent = stageEvent;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public Integer getNoOfSeats() {
+        return noOfSeats;
+    }
+
+    public void setNoOfSeats(Integer noOfSeats) {
+        this.noOfSeats = noOfSeats;
+    }
+
+    public void makePayment(double amount){}
+    public void makePayment(String walletNumber,double amount){}
+    public void makePayment(String creditCard,String ccv,String name,double amount){}
+}
