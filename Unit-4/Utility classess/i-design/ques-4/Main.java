c749a214bed65d5370fbf09fd7c8549b2fe242ff
Yash Raj <yash524767@gmail.com>
2024-04-01 12:25:00 +0530

    done

--- Unit-4/Utility classess/i-design/ques-4/Main.java
+++ Unit-4/Utility classess/i-design/ques-4/Main.java
@@ -1,12 +1,25 @@
 import java.util.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Dates: ");
-        String input[] = sc.nextLine().split(" ");
-        DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("ddMMyyyy");
 
+        System.out.println("Enter the Dates: ");
+        String input = sc.nextLine();
+        String[] dateStrings = input.split("\\s+");
+
+        DateTimeFormatter requiredFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.println("Formatted dates:");
+
+        StringBuilder formattedDates = new StringBuilder();
+        for (int i = 0; i < dateStrings.length; i++) {
+            LocalDate convert = LocalDate.parse(dateStrings[i], myFormatter);
+            formattedDates.append(convert.format(requiredFormatter)).append("\n");
+        }
+        
+        System.out.println(formattedDates.toString());
     }
 }
