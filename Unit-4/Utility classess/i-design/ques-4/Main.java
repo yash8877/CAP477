f4c57cc3da33841dee699e235c10eac256266cc8
Yash Raj <yash524767@gmail.com>
2024-04-01 12:55:16 +0530

    done

--- Unit-4/Utility classess/i-design/ques-4/Main.java
+++ Unit-4/Utility classess/i-design/ques-4/Main.java
@@ -7,12 +7,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("ddMMyyyy");
 
-        System.out.println("Enter the Dates: ");
+        System.out.println("Enter the Dates ");
         String input = sc.nextLine();
         String[] dateStrings = input.split("\\s+");
 
         DateTimeFormatter requiredFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        System.out.println("Formatted dates:");
+        System.out.println("Formatted dates ");
 
         StringBuilder formattedDates = new StringBuilder();
         for (int i = 0; i < dateStrings.length; i++) {
